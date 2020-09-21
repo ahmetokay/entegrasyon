7e8e9d233155e500027a61ca8929843731468b4e
ahmet <ahmet.okay@outlook.com>
2020-09-21 21:01:02 +0300

    OpenAPI version update

--- nvi-kimlik-dogrulama/src/test/java/tr/gov/csb/KisiServiceTest.java
+++ nvi-kimlik-dogrulama/src/test/java/tr/gov/csb/KisiServiceTest.java
@@ -4,30 +4,17 @@ import org.junit.Assert;
 import org.junit.Test;
 import tr.gov.csb.ws.KPSPublic;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 public class KisiServiceTest {
+
+    private KPSPublic kpsPublic = new KPSPublic();
 
     @Test
     public void dogruBilgi() {
-        try {
-            KPSPublic kpsPublic = new KPSPublic(new URL("https://tckimlik.nvi.gov.tr/Service/KPSPublic.asmx?WSDL"));
-
-            Assert.assertTrue(kpsPublic.getKPSPublicSoap().tcKimlikNoDogrula(16228427192L, "ahmet", "okay", 1988));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+        Assert.assertTrue(kpsPublic.getKPSPublicSoap().tcKimlikNoDogrula(16228427192L, "ahmet", "okay", 1988));
     }
 
     @Test
     public void yanlisBilgi() {
-        try {
-            KPSPublic kpsPublic = new KPSPublic(new URL("https://tckimlik.nvi.gov.tr/Service/KPSPublic.asmx?WSDL"));
-
-            Assert.assertFalse(kpsPublic.getKPSPublicSoap().tcKimlikNoDogrula(16228427192L, "ahmet", "okay", 1987));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+        Assert.assertFalse(kpsPublic.getKPSPublicSoap().tcKimlikNoDogrula(16228427192L, "ahmet", "okay", 1987));
     }
 }
